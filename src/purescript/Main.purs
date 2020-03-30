1b0068937726d0de3e0d88c55268e2c614d6f7a4
Giuliano.Santandrea <gsantandrea@imolainformatica.it>
2020-03-30 09:46:39 +0200

    added ffi

--- src/purescript/Main.purs
+++ src/purescript/Main.purs
@@ -19,20 +19,30 @@ import Effect.Console (error, log) as Console
 import Math (asin)
 import Web.DOM (Document) as DOM
 import Web.DOM.Document (createElement, toNonElementParentNode) as Web.DOM.Document
-import Web.DOM.Element (Element, setAttribute, setId, toEventTarget, toNode) as Web.DOM.Element
+import Web.DOM.Element (Element, setAttribute, setId, setClassName, toEventTarget, toNode) as Web.DOM.Element
 import Web.DOM.Node (appendChild, parentNode, removeChild, setTextContent) as Web.DOM.Node
 import Web.DOM.NonElementParentNode (getElementById) as Web.DOM.NonElementParentNode
 import Web.Event.Event (Event) as Event
 import Web.Event.EventTarget (addEventListener, eventListener) as Event
 import Web.HTML (window) as Web.HTML
 import Web.HTML.Event.EventTypes (click) as Event
-import Web.HTML.HTMLDocument (body, toDocument,HTMLDocument) as Web.HTML.HTMLDocument
+import Web.HTML.HTMLDocument (toDocument,body, toDocument,HTMLDocument) as Web.HTML.HTMLDocument
 import Web.HTML.HTMLElement (toNode,HTMLElement) as Web.HTML.HTMLElement
 import Web.HTML.HTMLInputElement (fromElement, value) as HTMLInput
 import Web.HTML.Window (document,Window) as Web.HTML.Window
 import Web.DOM.Internal.Types(Node) as Web.DOM.Internal.Types
 
-
+foreign import setStyleProp :: String -> String -> Web.DOM.Element.Element -> Effect Boolean
+createBoxElement :: String -> DOM.Document -> Effect Web.DOM.Element.Element
+createBoxElement id document = do
+    boxEl <-  Web.DOM.Document.createElement "div" document -- 
+    Web.DOM.Element.setId id boxEl
+    Web.DOM.Element.setClassName "box" boxEl
+    _ <- setStyleProp "position" "relative" boxEl
+    _ <- setStyleProp "width" "5em" boxEl
+    _ <- setStyleProp "height" "5em" boxEl
+    _ <- setStyleProp "background" "#ff4242" boxEl
+    pure boxEl
 
 
 label :: String -> DOM.Document -> Effect Web.DOM.Element.Element
@@ -53,11 +63,16 @@ main = do
   case maybeBody of
     Nothing   -> Console.error "no body element found!"
     Just (body::  Web.HTML.HTMLElement.HTMLElement) -> do
+      let bodyNode = (Web.HTML.HTMLElement.toNode :: Web.HTML.HTMLElement.HTMLElement -> Web.DOM.Internal.Types.Node)(body::  Web.HTML.HTMLElement.HTMLElement) 
+
+      defaultElem <- (Web.DOM.Document.createElement "span" document)
+      boxEl <- createBoxElement "the-box"  document
+      newBody <- Web.DOM.Node.appendChild (Web.DOM.Element.toNode boxEl) bodyNode
+
       container   <- Web.DOM.Document.createElement "section" document
       repoLabel   <- label "Subreddit1" document
       let containerNode = Web.DOM.Element.toNode container
       Web.DOM.Node.appendChild (Web.DOM.Element.toNode repoLabel) containerNode # void
-      let bodyNode = (Web.HTML.HTMLElement.toNode :: Web.HTML.HTMLElement.HTMLElement -> Web.DOM.Internal.Types.Node)(body::  Web.HTML.HTMLElement.HTMLElement) 
       Web.DOM.Node.appendChild containerNode bodyNode # void
       
 
